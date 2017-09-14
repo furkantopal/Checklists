bbf75edcd1edf44176fb7cec8c6837ead0039999
furkantopal <topallfurkan@gmail.com>
2017-09-14 12:19:06 +0300

    Add files via upload

---
+++ ChecklistItem.swift
@@ -0,0 +1,78 @@
+//
+//  File.swift
+//  Checklists
+//
+//  Created by Furkan Topal on 15.08.2017.
+//  Copyright © 2017 Furkan Topal. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class ChecklistItem: NSObject, NSCoding {
+    
+    var dueDate = Date()
+    var shouldRemind = false
+    var itemID : Int
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        text = aDecoder.decodeObject(forKey: "Text") as! String
+        checked = aDecoder.decodeBool(forKey: "Checked")
+        
+        dueDate = aDecoder.decodeObject(forKey: "DueDate") as! Date
+        shouldRemind = aDecoder.decodeBool(forKey: "ShouldRemind")
+        itemID = aDecoder.decodeInteger(forKey: "ItemID")
+        
+        super.init()
+    }
+    override init() {
+        itemID = DataModel.nextChecklistItemID()
+         super.init()
+    }
+    
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+    checked = !checked
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(text, forKey: "Text")
+        aCoder.encode(checked, forKey: "Checked")
+        
+        aCoder.encode(dueDate, forKey: "DueDate")
+        aCoder.encode(shouldRemind, forKey: "ShouldRemind")
+        aCoder.encode(itemID, forKey: "ItemID")
+    }
+    
+    func scheduleNotification() {
+        removeNotification()
+        if shouldRemind && dueDate > Date() {
+            let content = UNMutableNotificationContent()
+            content.title = "Reminder:"
+            content.body = text
+            content.sound = UNNotificationSound.default()
+            
+            let calendar = Calendar(identifier: .gregorian)
+            let components = calendar.dateComponents([.month, .day, .hour, .minute], from: dueDate)
+            
+            let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+            let request = UNNotificationRequest(identifier: "\(itemID)", content: content, trigger: trigger)
+            
+            let center = UNUserNotificationCenter.current()
+            center.add(request)
+            
+            print("Scheduled notification \(request) for itemID \(itemID)")
+        }
+    }
+    func removeNotification() {
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: ["\(itemID)"])
+    }
+    deinit {
+        removeNotification()
+    }
+}
+
